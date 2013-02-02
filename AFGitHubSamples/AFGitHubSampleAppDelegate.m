7772e80c271044983f14e80aa9775743c63fedec
Atsushi NAGASE <atsn.ngs@gmail.com>
2013-02-02 14:21:20 +0900

    Changed to use storyboard

--- AFGitHubSamples/AFGitHubSampleAppDelegate.m
+++ AFGitHubSamples/AFGitHubSampleAppDelegate.m
@@ -27,19 +27,11 @@
 
 @implementation AFGitHubSampleAppDelegate
 
-- (BOOL)application:(UIApplication *)application
-didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:8 * 1024 * 1024 diskCapacity:20 * 1024 * 1024 diskPath:nil];
     [NSURLCache setSharedURLCache:URLCache];
     
     [[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
-    
-    UIViewController *viewController = [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
-    self.navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
-    
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.rootViewController = self.navigationController;
-    [self.window makeKeyAndVisible];
     
     return YES;
 }
